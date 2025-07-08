f8e49f5c4ca74a45541529deb4479923d1e60e11
Alexpi2050 <sebastianpiedra019@gmail.com>
2025-07-08 12:20:01 -0500

    Update playlist_page.h

--- playlist_page.h
+++ playlist_page.h
@@ -5,50 +5,49 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
-#include "generalH.h" // Para las estructuras del proyecto principal
 
-typedef enum {
-    PLAYLIST_STATE_IDLE,
-    PLAYLIST_STATE_PLAYING,
-    PLAYLIST_STATE_HOVER
-} PlaylistState;
-
-// Estructura de la página de playlist
+// Colores
 typedef struct {
-    // Recursos gráficos
+    SDL_Color fondo;
+    SDL_Color blanco;
+    SDL_Color gris;
+    SDL_Color texto;
+    SDL_Color hover;
+} PlaylistColors;
+
+// Estructura principal
+typedef struct {
+    // Recursos
     SDL_Texture* background;
     SDL_Texture* albumCover;
+    TTF_Font* fontTitle;
+    TTF_Font* fontDetails;
     
-    // Fuentes
-    TTF_Font* fontTitle; // Título grande (60px)
-    TTF_Font* fontMetadata; // Metadatos (16px)
-    TTF_Font* fontSongs; // Lista de canciones (16px)
+    // Datos
+    const char* playlistName;
+    const char* playlistAuthor;
+    int songCount;
+    SDL_Rect* songRects;
     
-    // Datos dinámicos
-    playlist* currentPlaylist;
-    playlistNodo* currentlyPlaying; // Canción en reproducción
-    SDL_Rect songItems[50]; // Áreas clickeables de canciones
-    int visibleSongCount; // Canciones renderizadas
-    
-    SDL_Rect backButton; // Botón de retroceso
-    PlaylistState state;
-    int hoveredItemIndex; // Índice de canción hover
-    
-    SDL_Color colorNormal;
-    SDL_Color colorHover;
-    SDL_Color colorPlaying;
+    // Interacción
+    SDL_Rect backButton;
+    SDL_Rect nextButton;
+    bool showNextButton;
+    int hoveredItem;
+    PlaylistColors colors;
 } PlaylistPage;
 
-void Playlist_Init(SDL_Renderer* renderer, playlist* pl);
+// Inicialización
+void Playlist_Init(SDL_Renderer* renderer, const char* name, const char* author, int totalSongs);
 
-void Playlist_Render(SDL_Renderer* renderer, playlistNodo* currentSong);
+// Renderizado
+void Playlist_Render(SDL_Renderer* renderer, const char** titles, const char** artists, 
+                    const char** albums, const char** durations);
 
-playlistNodo* Playlist_HandleEvent(SDL_Event* event, AppState* appState);
+// Eventos
+void Playlist_HandleEvent(SDL_Event* event, bool* running);
 
+// Limpieza
 void Playlist_Destroy();
 
-void Playlist_RenderText(SDL_Renderer* renderer, TTF_Font* font, const char* text, int x, int y, SDL_Color color);
-
-void Playlist_UpdateScroll(int scrollOffset);
-
-#endif // PLAYLIST_PAGE_H
+#endif
