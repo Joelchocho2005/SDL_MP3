f5377ead8f370cd2f062f1601c3d1ae9b0e14b3a
Alexpi2050 <sebastianpiedra019@gmail.com>
2025-07-10 12:38:47 -0500

    Update playlist_page.h

--- playlist_page.h
+++ playlist_page.h
@@ -16,31 +16,24 @@ typedef struct {
 } PlaylistColors;
 
 typedef struct {
-    // Recursos
     SDL_Texture* background;
     SDL_Texture* albumCover;
     TTF_Font* fontTitle;
     TTF_Font* fontDetails;
     
-    // Datos
     const char* playlistName;
     const char* playlistAuthor;
     cancionInfo* canciones;
     int totalCanciones;
     int capacidad;
     
-    // Interacción
     SDL_Rect backButton;
     SDL_Rect nextButton;
-    SDL_Rect prevButton;
     bool showNextButton;
-    bool showPrevButton;
     int hoveredItem;
-    int paginaActual;
     PlaylistColors colors;
 } PlaylistPage;
 
-// Funciones
 void Playlist_Init(SDL_Renderer* renderer, const char* name, const char* author);
 void Playlist_AgregarCancion(PlaylistPage* page, const cancionInfo* cancion);
 void Playlist_Render(SDL_Renderer* renderer, PlaylistPage* page);
